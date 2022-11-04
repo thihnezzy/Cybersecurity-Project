6b10850d65d7681f95adbc203c2ca71a29f386d9
Ghita <bentaibighita@gmail.com>
2022-11-04 12:02:21 +0100

    msg

---
+++ client/src2/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Products from './components/Products';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+   <>
+   
+   <Navbar/>
+   <Home />
+   <Products />
+   <Footer />
+
+   </>
+  );
+}
+
+export default App;
---
+++ client/src2/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div className='hero'>
+
+            <div class="card bg-dark text-white border-0">
+                <img src="/assets/nike.PNG" class="card-img" alt="..." height="550px"/>
+                <div class="card-img-overlay d-flex flex-column justify-content">
+                    <div className="container">
+                    <h5 class="card-title display-3 fw-bolder mb-0">NEW ARRIVALS</h5>
+                    <p class="card-text lead fs-2 fw-bolder">CHECK OUT THE TRENDS</p>
+
+                    </div>
+                  
+                </div>
+            </div>
+
+        </div>
+
+
+    );
+
+}
+
+export default Home;
