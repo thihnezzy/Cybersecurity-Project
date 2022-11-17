5542ef88a81e050bc79384e11435cfa42d8e72fb
n-salma <salmanayab0@gmail.com>
2022-11-17 09:21:47 +0100

    checkout progress

--- client/src/components/CheckoutForm/CustomTextField.jsx
+++ client/src/components/CheckoutForm/CustomTextField.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { useFormContext, Controller } from 'react-hook-form';
 import { TextField, Grid } from '@material-ui/core';
+import { useFormContext, Controller } from 'react-hook-form';
 
-function FormInput({ name, label, required }) {
-  const { control } = useFormContext();
-  const isError = false;
 
-  return (
-    <Grid item xs={12} sm={6}>
-      <Controller
-        as={TextField}
-        name={name}
-        control={control}
-        label={label}
-        fullWidth
-        required={required}
-        error={isError}
-      />
-    </Grid>
-  );
-}
+const FormInput = ({ name, label, required}) => {
+const { control } = useFormContext();
+   const isError = false;
+
+return (
+   <>
+         <Controller
+            control={control}
+            name={name}
+            render = {({ field})=> (
+                <TextField
+                    fullWidth
+                    label={label}
+                    required
+                />
+            )}
+         />
+   </>
+ );
+ }
 
 export default FormInput;
--- client/src/components/CheckoutForm/PaymentForm.jsx
+++ client/src/components/CheckoutForm/PaymentForm.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-const PaymentForm = () => {
+import Review from './Review';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const PaymentForm = ({ checkoutToken }) => {
+  
+    
+  
+
   return (
-    <div>PaymentForm</div>
+    <>
+      <Review checkoutToken={checkoutToken} />
+      <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment method</Typography>
+      
+    </>
   );
-}
+};
 
-export default PaymentForm
+export default PaymentForm;
